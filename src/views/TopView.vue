e5c13b19cfd6349a05cd82b309acd5be56311c82
fujisann1024 <tehepero302@gmail.com>
2023-04-22 21:26:23 +0900

    不要ファイルの削除、HomeView→TopViewへ変更、vueの修正

---
+++ src/views/TopView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1>This is an top page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Top"
+});
+</script>
